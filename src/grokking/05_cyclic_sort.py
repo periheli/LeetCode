9c0b4ae728febaeaf375b93e37bc5cc596f37580
peri <76472728+periheli@users.noreply.github.com>
2025-09-23 00:15:12 +0700

    grok: cyclic sort 2

--- src/grokking/05_cyclic_sort.py
+++ src/grokking/05_cyclic_sort.py
@@ -88,3 +88,67 @@ class Solution:
                 nums[i], nums[j] = nums[j], nums[i]
         duplicates = [num for i, num in enumerate(nums) if i != (num - 1)]
         return duplicates
+
+    def find_corrupt_numbers(self, nums: list[int]) -> list[int]:
+        for i in range(len(nums)):
+            while nums[i] != nums[nums[i] - 1]:
+                j = nums[i] - 1
+                nums[i], nums[j] = nums[j], nums[i]
+        for i, num in enumerate(nums):
+            if i != (num - 1):
+                return [num, i + 1]
+        return [-1, -1]
+
+    def find_smallest_missing_positive(self, nums: list[int]) -> int:
+        n = len(nums)
+        for i in range(len(nums)):
+            while (1 <= nums[i] <= n) and (nums[i] != nums[nums[i] - 1]):
+                j = nums[i] - 1
+                nums[i], nums[j] = nums[j], nums[i]
+        for i, num in enumerate(nums):
+            if i != (num - 1):
+                return i + 1
+        return n + 1
+
+    def find_first_k_missing_positive(
+        self, nums: list[int], k: int
+    ) -> list[int]:
+        nums += [0] * k
+        n = len(nums)
+        for i in range(len(nums)):
+            while (1 <= nums[i] <= n) and (nums[i] != nums[nums[i] - 1]):
+                j = nums[i] - 1
+                nums[i], nums[j] = nums[j], nums[i]
+        missing_numbers = []
+        for i, num in enumerate(nums):
+            if i != (num - 1):
+                missing_numbers.append(i + 1)
+                if len(missing_numbers) == k:
+                    return missing_numbers
+        return missing_numbers + list(
+            range(n + 1, n + 1 + (k - len(missing_numbers)))
+        )
+
+    def find_first_k_missing_positive(
+        self, nums: list[int], k: int
+    ) -> list[int]:
+        n = len(nums)
+        for i in range(len(nums)):
+            while (1 <= nums[i] <= n) and (nums[i] != nums[nums[i] - 1]):
+                j = nums[i] - 1
+                nums[i], nums[j] = nums[j], nums[i]
+        missing_numbers = []
+        extra_nums = set()
+        for i, num in enumerate(nums):
+            if i != (num - 1):
+                missing_numbers.append(i + 1)
+                if (n + 1) <= num <= (n + k):
+                    extra_nums.add(num)
+                if len(missing_numbers) == k:
+                    return missing_numbers
+        missing_num = n + 1
+        while len(missing_numbers) < k:
+            if missing_num not in extra_nums:
+                missing_numbers.append(missing_num)
+            missing_num += 1
+        return missing_numbers
